be739891298bf9583d7f3c88081fb5a03e6a7584
wkmnet <wkmwkm@sina.com>
2015-01-16 19:00:12 +0800

    各设备密钥生成工具

--- src/test/java/cn/pay/wkm/common/tool/hsm/CreateDeviceKey.java
+++ src/test/java/cn/pay/wkm/common/tool/hsm/CreateDeviceKey.java
@@ -14,9 +14,9 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.commons.lang.time.DateFormatUtils;
 import org.apache.log4j.Logger;
 import org.junit.Before;
-import org.junit.Test;
 
 /**
  * ClassName:CreateDeviceKey <br/>
@@ -51,7 +51,6 @@ public class CreateDeviceKey {
 	 * @author wkmnet@foxmail.com
 	 * @since JDK 1.6
 	 */
-	@Test
 	public void mobilepay(){
 		StringBuilder builer = new StringBuilder();
 		StringBuilder builerNext = new StringBuilder();
@@ -125,7 +124,170 @@ public class CreateDeviceKey {
 		toLocation(sql.toString(), keyDir.getAbsolutePath() + "/tmk_sql.txt");
 	}
 	
+	/**
+	 * 爱创机型密钥生成
+	 * itron:(这里用一句话描述这个方法的作用). <br/>
+	 * TODO(这里描述这个方法适用条件 – 可选).<br/>
+	 * TODO(这里描述这个方法的执行流程 – 可选).<br/>
+	 * TODO(这里描述这个方法的使用方法 – 可选).<br/>
+	 * TODO(这里描述这个方法的注意事项 – 可选).<br/>
+	 *
+	 * @author wkmnet@foxmail.com
+	 * @since JDK 1.6
+	 */
+	public void itron(){
+		String OUTPUT_DIR = "E:/keys/itron_key";
+		SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
+		File dir = new File(OUTPUT_DIR);
+		File current = new File(dir,format.format(new Date()));
+		if(!current.exists()){
+			current.mkdirs();
+		}
+		KeyInfo key = new KeyInfo();
+		tool.create3DESTMK(key);
+		toLocation(key.getKeyValue(),new File(current,"tmk.txt").getAbsolutePath());
+		itronTAK(current,key.getKeyValue());
+	}
+	private void itronTAK(File dir,String tpk){
+		String clearTpk = tpk;
+		String pre = "8000";
+		StringBuilder itron = new StringBuilder();
+		StringBuilder sql = new StringBuilder();
+		StringBuilder clearTAK= new StringBuilder();
+		for(int i=5001; i < 15001;i++){
+			String ksn = "000" + String.format("%05d", i);
+			KeyInfo key = new KeyInfo();
+			tool.create3DESTAKByTMK(clearTpk,key);
+			//生成爱创所需密钥格式
+			//itron.append(pre); 2015年1月12日确定不加8000
+			itron.append(ksn);
+			itron.append("=");
+			itron.append(key.getKeyCiphertext());
+			itron.append(" " + key.getCheckValue());
+			itron.append(System.getProperty("line.separator"));
+			//生成中汇密钥入库格式
+			sql.append(itronCreateSQL(pre + ksn,key.getCheckValue(),tool.encrypt3DESTAKByLMK(key.getKeyValue())));
+			clearTAK.append(pre + ksn);
+			clearTAK.append(",");
+			clearTAK.append(key.getKeyValue());
+			clearTAK.append(",");
+			clearTAK.append(key.getCheckValue());
+			clearTAK.append(System.getProperty("line.separator"));
+		}
+		//写入文件
+		toLocation(itron.toString(),new File(dir,"key.txt").getAbsolutePath());
+		sql.append("commit;");
+		toLocation(sql.toString(),new File(dir,"tak_store.sql").getAbsolutePath());
+		toLocation(clearTAK.toString(),new File(dir,"clear_key.sql").getAbsolutePath());
+	}
+	private String itronCreateSQL(String ksn,String checkValue,String keyValue){
+		StringBuilder sql = new StringBuilder();
+		sql.append("insert into mcr_ksn (id,date_created,is_activated,is_used,ksn_no) values (seq_mcrksn.nextval,systimestamp,0,0,'");
+		sql.append(ksn);
+		sql.append("');");
+		sql.append(System.getProperty("line.separator"));
+		
+		sql.append("insert into key_store  (id,check_value,class_name,key_alias,key_length,key_type,key_value)values (seq_keystore.nextval,'");
+		sql.append(checkValue);
+		sql.append("','org.jpos.security.SecureDESKey','ws.");
+		sql.append(ksn);
+		sql.append(".tak',128,'TAK','");
+		sql.append(keyValue);
+		sql.append("');");
+		sql.append(System.getProperty("line.separator"));
+		return sql.toString();
+	}
 	
+	/**
+	 * 宜信密钥生成,只针对宜信机型
+	 * creditease:(这里用一句话描述这个方法的作用). <br/>
+	 * TODO(这里描述这个方法适用条件 – 可选).<br/>
+	 * TODO(这里描述这个方法的执行流程 – 可选).<br/>
+	 * TODO(这里描述这个方法的使用方法 – 可选).<br/>
+	 * TODO(这里描述这个方法的注意事项 – 可选).<br/>
+	 *
+	 * @author wkmnet@foxmail.com
+	 * @since JDK 1.6
+	 */
+	public void creditease(){
+		
+		String workDir = "E:/keys/yixin_key";
+		
+		String currentTime = DateFormatUtils.format(new Date(), "yyyyMMddHHmmss");
+		
+		File work = new File(workDir);
+		if(!work.exists() || !work.isDirectory()){
+			logger.error("不存在目录:" + workDir);
+			return ;
+		}
+		File currentDir = new File(work,currentTime);
+		
+		if(!currentDir.exists()){
+			logger.info("创建目录:" + currentDir.getAbsolutePath());
+			currentDir.mkdir();
+		}
+		crediteaseCreateKey(currentDir);
+	}
+	private void crediteaseCreateKey(File dir){
+		StringBuilder posKey = new StringBuilder();
+		StringBuilder sql = new StringBuilder();
+		StringBuilder sqlTest = new StringBuilder();
+		
+		for(int i = 3001;i <= 4519;i++) {
+			String ksn = "1710000100002000" + String.format("%04d", i);
+			KeyInfo tmk = new KeyInfo();
+			tool.create3DESTMK(tmk);;
+			posKey.append(crediteaseCreateKey(ksn,tmk));
+			
+			sql.append(crediteaseCreateKsnSQL(ksn, tmk));
+			sql.append(crediteaseCreateKsnAgency(ksn, "PM13"));
+			
+			sqlTest.append(crediteaseCreateKsnSQL(ksn, tmk));
+			sqlTest.append(crediteaseCreateKsnAgency(ksn, "YIXIN_TEST"));
+		}
+		
+		toLocation(posKey.toString(), new File(dir,"pos_id.txt"));
+		sql.append("commit;");
+		toLocation(sql.toString(), new File(dir,"yixin_shangxian.txt"));
+		sqlTest.append("commit;");
+		toLocation(sqlTest.toString(), new File(dir,"yixin_test.txt"));
+	}
+	private String crediteaseCreateKey(String ksn,KeyInfo tmk){
+		StringBuilder key = new StringBuilder();
+		key.append(ksn);
+		key.append(",");
+		key.append(tmk.getKeyValue());
+		key.append(tmk.getCheckValue());
+		key.append(System.getProperty("line.separator"));
+		return key.toString();
+	}
+	private String crediteaseCreateKsnSQL(String ksn,KeyInfo tmk){
+		StringBuilder sql = new StringBuilder();
+		sql.append("insert into mcr_ksn (id,date_created,is_activated,is_used,ksn_no) values (seq_mcrksn.nextval,systimestamp,0,0,'");
+		sql.append(ksn);
+		sql.append("');");
+		sql.append(System.getProperty("line.separator"));
+		
+		sql.append("insert into key_store  (id,check_value,class_name,key_alias,key_length,key_type,key_value)values (seq_keystore.nextval,'");
+		sql.append(tmk.getCheckValue());
+		sql.append("','org.jpos.security.SecureDESKey','ws.");
+		sql.append(ksn);
+		sql.append(".tmk',128,'TMK','");
+		sql.append(tool.encrypt3DESTMKByLMK(tmk.getKeyValue()));
+		sql.append("');");
+		sql.append(System.getProperty("line.separator"));
+		return sql.toString();
+	}
+	private String crediteaseCreateKsnAgency(String ksn,String agencyCode){
+		StringBuilder sql = new StringBuilder();
+		sql.append("insert into agency_ksn (ksn_no,agency_code) values ('");
+		sql.append(ksn);
+		sql.append("','");
+		sql.append(agencyCode);
+		sql.append("');");
+		sql.append(System.getProperty("line.separator"));
+		return sql.toString();
+	}
 	
 	
 	
